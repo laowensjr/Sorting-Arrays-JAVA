b4f43c55d67d13797fac88de536d891cfb710559
laowensjr <laowensjr@gmail.com>
2020-01-18 19:39:13 -0600

    Update CoffeeDriver.java

--- CoffeeDriver.java
+++ CoffeeDriver.java
@@ -24,11 +24,11 @@ public class CoffeeDriver
  String k;
  double j;
 
- itemName[0] = “Coffee”;
- itemName[1] = “Water”;
- itemName[2] = “Milk”;
- itemName[3] = “Bagel”;
- itemName[4] = “Donut”;
+ itemName[0] = "Coffee";
+ itemName[1] = "Water";
+ itemName[2] = "Milk";
+ itemName[3] = "Bagel";
+ itemName[4] = "Donut";
 
  itemPrice[0] = 1.00;
  itemPrice[1] = 2.00;
@@ -36,17 +36,17 @@ public class CoffeeDriver
  itemPrice[3] = 1.25;
  itemPrice[4] = 0.75;
 
- prompt = JOptionPane.showInputDialog(null, “Welcome to Wings Coffee Shop. We have a great list items on our menu. \nWould you like to see these items sorted by name or by price? (n / p): “,
- “Please select your sort Method”, JOptionPane.QUESTION_MESSAGE);
+ prompt = JOptionPane.showInputDialog(null, "Welcome to Wings Coffee Shop. We have a great list items on our menu. \nWould you like to see these items sorted by name or by price? (n / p): ",
+ "Please select your sort Method", JOptionPane.QUESTION_MESSAGE);
  prompt.toLowerCase();
- if(prompt.equals(“n”))
+ if(prompt.equals("n"))
  sort(itemName);
- JOptionPane.showMessageDialog(null, “The values are: $ ” + itemName);
+ JOptionPane.showMessageDialog(null, "The values are: $ " + itemName);
 
 
- if (prompt.equals(“p”))
+ if (prompt.equals("p"))
  sort(itemPrice);
- JOptionPane.showMessageDialog(null, “The values are: $ ” + itemPrice);
+ JOptionPane.showMessageDialog(null, "The values are: $ " + itemPrice);
 
 
  }
